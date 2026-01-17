e653e0c9e6546afdd857c4df64d8d008477237d6
Ekrem Bas <ekrembas03@outlook.com>
2026-01-17 18:32:06 +0300

    add: hive integration in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:limitim/models/expense.dart';
+import 'package:limitim/models/month.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(MonthAdapter());
+  Hive.registerAdapter(ExpenseAdapter());
+
+  await Hive.openBox<Month>("months");
+  await Hive.openBox<Expense>("expenses");
+
   runApp(const MyApp());
 }
 
